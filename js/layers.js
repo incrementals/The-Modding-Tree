976b696fa0639b7c327aad20e709e3dc3525a76d
incrementals <49661996+incrementals@users.noreply.github.com>
2021-07-06 17:47:12 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -44,7 +44,7 @@ addLayer("g", {
         13: {
             title: "Use free models",
             description: "Lowers robux requirement to make a game.",
-            cost: new Decimal(350),
+            cost: new Decimal(125),
             effect() {
                 return softcap(player[this.layer].points.add(1).pow(0.08),new Decimal(2),0.02)
             },
